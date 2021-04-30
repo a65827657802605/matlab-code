ba80d733b7e2f5b460b217cabb682d6a30b340a7
VFransen <vito@sjoux.com>
2021-04-30 12:26:45 +0200

    Insertion Olivia's script into RVE

--- ReferenceValueEditor.m
+++ ReferenceValueEditor.m
@@ -6,10 +6,20 @@
 %Input
 %See the image for a definition of the coordinate systems
 
-x                       = -5; %[m] x postition as defined in figure
-y                       = 5; %[m] y postition as defined in figure
+x                       = -0.3; %[m] x postition as defined in figure
+y                       = 0.3; %[m] y postition as defined in figure
 robot_to_conveyorbelt   = 0; %[m] distance from the robot to the conveyorbelt
-h                       = 0; %[m] desired height
+h                       = 0.1; %[m] desired height
+
+%See Rutger's drawing
+gripper_height          = 0; %[m] distance between the gripper attachment point and the robot arm
+AB                      = 0.081; %[m]
+BC                      = 0.165; %[m]
+BG                      = 0.080; %[m]
+CD                      = 0.130; %[m]
+DE                      = 0.120; %[m]
+EF                      = 0.065; %[m]
+GE                      = 0.130; %[m]
 
 %%
 %Conversion from (x,y) to (R,r)
@@ -20,8 +30,28 @@ R                       = atan( x / y ); %Standard arctangent rule
 
 %%
 %Conversion from (r,h) to (X,Z)
-X=0.3;
-Z=0;
+
+AC                      = BC + AB;
+DF                      = DE + EF;
+
+CF                      = sqrt( r * r + (AC - h + gripper_height) ^ 2 ); 
+phi1                    = atan( (AC - h + gripper_height) / r );
+phi2                    = acos( (CF^2 + DF^2 - CD^2) / (2 * CF * DF) );
+phi3                    = pi / 2 - phi1 - phi2;
+Dx                      = r - DF * sin(phi3);
+Dy                      = h + DF * cos(phi3);
+
+Dy1                     = Dy - (AB - AC);
+phi4                    = atan( (r - Dx) / (Dy - h + gripper_height) );
+Ex                      = r - EF * sin(phi4);
+Ey                      = EF * cos(phi4) - h;
+
+Ey1                     = Ey - AB;
+BE                      = sqrt( Ex^2 + Ey1^2 );
+
+Z                       = pi / 2 - acos( Dx / CD );
+X                       = pi / 2 - atan( Ey1 / Ex ) - acos( (BG^2 + BE^2 - GE^2) / 2 / BG / BE);
+
 %%
 %Check if constraints are met
 
